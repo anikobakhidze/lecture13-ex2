22ba1080d1369b8c9268daa95a7c5e90fbdf4948
anikobakhidze <kobakhidzeana23@gmail.com>
2022-12-21 15:17:00 +0400

    add positive and negative array

---
+++ index.js
@@ -0,0 +1,5 @@
+let arr=[3,5,12,45,10,-10,-101,2,-200,47,89];
+let positiveArr= arr.filter(el => el>0);
+console.log(positiveArr);
+let negativeArr= arr.filter(el => el <0 );
+console.log(negativeArr);
